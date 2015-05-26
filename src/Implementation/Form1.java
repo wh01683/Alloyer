7cae32211960659bc1ebe3c2865069c0746ee763
ld02440 <ld02440@georgiasouthern.edu>
2015-05-26 13:52:51 -0400

    gui

---
+++ src/Implementation/Form1.java
@@ -0,0 +1,27 @@
+package Implementation;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Lindsey on 5/26/2015.
+ */
+public class Form1
+{
+    private JPanel panel1;
+    private JButton createSignatureButton;
+    private JTextField labelSig;
+    private JRadioButton primaryRadioButton;
+    private JRadioButton subsetRadioButton;
+    private JCheckBox abstractCheckBox;
+    private JButton createSigButton;
+
+
+    public Form1()
+    {
+
+    }
+}
+
+
