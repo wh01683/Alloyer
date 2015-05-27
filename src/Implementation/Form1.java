d1990db013c332b0db0040b2d1880bdab07517d3
Robert Howerton <wh01683@georgiasouthern.edu>
2015-05-27 10:30:21 -0400

    creating Domain class to house information from which to create models

--- src/Implementation/Form1.java
+++ src/Implementation/Form1.java
@@ -1,9 +1,5 @@
 package Implementation;
 
-import edu.mit.csail.sdg.alloy4.Err;
-import edu.mit.csail.sdg.alloy4compiler.ast.Attr;
-import edu.mit.csail.sdg.alloy4compiler.ast.Sig;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,60 +10,31 @@ import java.awt.event.ActionListener;
 public class Form1
 {
     private JPanel panel1;
+    private JButton createSignatureButton;
     private JTextField txtSigName;
     private JRadioButton primaryRadioButton;
     private JRadioButton subsetRadioButton;
     private JCheckBox abstractCheckBox;
+    private JButton createSigButton;
     private JComboBox cmbMultiplicity;
     private JButton btnCreateSig;
-    private JList lstSigs;
+    private JTable table1;
 
 
     public Form1()
     {
-        btnCreateSig.addActionListener(new ActionListener() {
+        createSigButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                createSig();
             }
         });
+
+
+
     }
 
-    private Sig createSig(){
 
-        String multiplicitySelection = String.valueOf(cmbMultiplicity.getSelectedItem());
-        try {
-            if (primaryRadioButton.isSelected()) {
-                Sig sig = new Sig.PrimSig(txtSigName.getText().toString(),
-                        getMultiAttr(multiplicitySelection),
-                        subsetRadioButton.isSelected() ? Attr.SUBSET : null,
-                        abstractCheckBox.isSelected() ? Attr.ABSTRACT : null);
-                return sig;
 
-            }else{
-                //ToDo: implement subset sig creation
-                return null;
-            }
-        }catch (Err e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private Attr getMultiAttr(String selection){
-        if(selection.equalsIgnoreCase("LONE")){
-            return Attr.LONE;
-        }
-        else if(selection.equalsIgnoreCase("ONE")){
-            return Attr.ONE;
-        }
-        else if (selection.equalsIgnoreCase("SOME")){
-            return Attr.SOME;
-        }
-        else{
-            return null;
-        }
-    }
 }
 
 
