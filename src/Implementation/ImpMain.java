32595393cf6eba491680f44a7f9a8dc393327940
Robert Howerton <wh01683@georgiasouthern.edu>
2015-05-29 14:05:00 -0400

    working on a test domain

--- src/Implementation/ImpMain.java
+++ src/Implementation/ImpMain.java
@@ -1,15 +1,29 @@
 package Implementation;
 
+import edu.mit.csail.sdg.alloy4.Err;
+
 /**
  * Created by robert on 5/29/15.
  */
 public class ImpMain {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Err {
 
         Form1 form1 = new Form1();
 
 
+        TestDomain x;
+        x = new TestDomain();
+
+        x.makeDomain();
+        x.makeInstance1();
+
+        x.runFor3(x.file.some());
+
+        x = new TestDomain();
+        x.makeDomain();
+        x.makeInstance1();
+        x.runFor3(TestDomain.acyclic(x.contains).not());
 
     }
 
