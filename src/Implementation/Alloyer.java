c699972db03b0c6889c3df5b922c37a4be345036
ld02440 <ld02440@georgiasouthern.edu>
2015-06-12 18:01:15 -0400

    new gui

--- src/Implementation/Alloyer.java
+++ src/Implementation/Alloyer.java
@@ -1,8 +1,15 @@
 package Implementation;
 
+import javax.swing.*;
+
 /**
  * Created by Lindsey on 6/11/2015.
  */
 public class Alloyer
 {
+    private JPanel panel1;
+    private JLabel Alloyer;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
 }
