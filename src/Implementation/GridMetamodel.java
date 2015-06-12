f5c1ab9f2c3099ff2e9e6cfc0f0f8f5ac69d349d
Robert Howerton <wh01683@georgiasouthern.edu>
2015-06-11 20:31:59 -0400

    great success

--- src/Implementation/GridMetamodel.java
+++ src/Implementation/GridMetamodel.java
@@ -11,6 +11,7 @@ import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
 import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static edu.mit.csail.sdg.alloy4.A4Reporter.NOP;
@@ -42,7 +43,7 @@ public class GridMetamodel {
         Sig.PrimSig Circuit = new Sig.PrimSig("Circuit", Attr.ABSTRACT);
 
         //circuit: some Circuit in Grid
-        Expr circuit = Grid.addField("circuit", Circuit.someOf());
+        Expr field_circuit_GRID = Grid.addField("circuit", Circuit.someOf());
 
         /*sig Load_Circuit extends Circuit*/
         Sig.PrimSig LoadCircuit = new Sig.PrimSig("Load_Circuit", Circuit);
@@ -51,19 +52,19 @@ public class GridMetamodel {
         Sig.PrimSig SupplyCircuit = new Sig.PrimSig("Supply_Circuit", Circuit);
 
         /*supply_circuit: set Supply_Circuit in Load_Circuit*/
-        Expr supply_circuit = LoadCircuit.addField("supply_circuit", SupplyCircuit.setOf());
+        Expr field_supply_circuit_LOAD_CIRCUIT = LoadCircuit.addField("supply_circuit", SupplyCircuit.setOf());
         /*load_circuit: set Load_Circuit in Supply_Circuit*/
-        Expr load_circuit = SupplyCircuit.addField("load_circuit", LoadCircuit.setOf());
+        Expr field_load_circuit_SUPPLY_CIRCUIT = SupplyCircuit.addField("load_circuit", LoadCircuit.setOf());
 
         /*abstract sig Component*/
         Sig.PrimSig Component = new Sig.PrimSig("Component", Attr.ABSTRACT);
         /*containing_circuit: one Circuit*/
-        Expr containing_circuit = Component.addField("containing_circuit", Circuit.oneOf());
+        Expr field_containing_circuit_COMPONENT = Component.addField("containing_circuit", Circuit.oneOf());
 
         /*sig Switch extends Component*/
         Sig.PrimSig Switch = new Sig.PrimSig("Switch", Component);
         /*switch: Circuit -> Circuit*/
-        Expr switchComp = Switch.addField("switch", Circuit.some_arrow_some(Circuit));
+        Expr field_switch_CIRCUIT = Switch.addField("switch", Circuit.some_arrow_some(Circuit));
 
 
         /*abstract sig Supply extends Component*/
@@ -73,16 +74,16 @@ public class GridMetamodel {
         Sig.PrimSig Load = new Sig.PrimSig("Load", Component);
 
         /*load: set Load in Supply*/
-        Expr load = Supply.addField("load", Load.setOf());
+        Expr field_load_SUPPLY = Supply.addField("load", Load.setOf());
 
         /*watts: one Int in Supply*/
-        Expr load_watts = Supply.addField("watts", SIGINT.oneOf());
+        Expr field_watts_SUPPLY = Supply.addField("watts", SIGINT.oneOf());
 
         /*supply: one Supply in Load*/
-        Expr supply = Load.addField("supply", Supply.oneOf());
+        Expr field_supply_LOAD = Load.addField("supply", Supply.oneOf());
 
         /*watts: one Int in Load*/
-        Expr supply_watts = Supply.addField("watts", SIGINT.oneOf());
+        Expr field_watts_LOAD = Supply.addField("watts", SIGINT.oneOf());
 
         /*sig GP extends Supply*/
         Sig.PrimSig GP = new Sig.PrimSig("GP", Supply);
@@ -100,64 +101,50 @@ public class GridMetamodel {
         Sig.PrimSig Hydro = new Sig.PrimSig("Hydro", Supply);
 
 
-        Decl l = Load.oneOf("l");
-        Decl s = Supply.oneOf("s");
-        Decl g = Grid.oneOf("g");
-        Decl circ = Circuit.oneOf("c");
-
-
-        Decl c_supply = Circuit.join(supply).oneOf("c.supply");
-        Decl c_load = Circuit.join(load).oneOf("c.load");
-
-        Decl g_circuit_load = Grid.join(circuit).join(load).oneOf("g.circuit.load");
-        Decl g_circuit_supply = Grid.join(circuit).join(supply).oneOf("g.circuit.supply");
-
-        Decl l_watts = Load.join(load_watts).oneOf("l.watts");
-        Decl s_watts = Supply.join(supply_watts).oneOf("s.watts");
-
-
-
-
 
 
         /*fun s_watts [c: Circuit]: one Int
         *   sum s: c.supply | s.watts*/
-        Decl sup = Circuit.join(supply).oneOf("s");
-        Expr s_watts_body = supply_watts.sumOver(sup, c_supply);
-        Func fun_s_watts = new Func(null, "s_watts", Util.asList(circ), SIGINT.oneOf(), s_watts_body);
+
+        Decl decl_oneOf_Circuit = Circuit.oneOf("c");
+        Decl decl_circuit_supply = decl_oneOf_Circuit.get().join(field_supply_LOAD).oneOf("s");
+
+        Expr s_watts_body = decl_circuit_supply.get().join(field_watts_SUPPLY).sumOver(decl_circuit_supply);
+        Func fun_s_watts = new Func(null, "s_watts", Util.asList(), SIGINT.oneOf(), s_watts_body);
 
         /*fun l_watts [c: Circuit]: one Int
         *   sum l: c.load | l.watts*/
-
-        Decl load1 = Circuit.join(load).oneOf("l");
-        Expr l_watts_body = load_watts.sumOver(load1, circ);
-        Func fun_l_watts = new Func(null, "l_watts", Util.asList(circ), SIGINT.oneOf(), l_watts_body);
+        Decl decl_c_load = decl_oneOf_Circuit.get().join(field_load_SUPPLY).oneOf("l");
+        Expr l_watts_body = decl_c_load.get().join(field_watts_LOAD).sumOver(decl_c_load);
+        Func fun_l_watts = new Func(null, "l_watts", Util.asList(decl_oneOf_Circuit), SIGINT.oneOf(), l_watts_body);
 
         /*fun grid_l_watts [g: Grid]: one Int
         *   sum l: g.circuit.load | l.watts*/
 
-        Decl gr = Grid.join(circuit).join(load).oneOf("l");
-        Expr grid_l_watts_body = load_watts.sumOver(gr, g);
-        Func fun_grid_l_watts = new Func(null, "grid_l_watts", Util.asList(g), SIGINT.oneOf(), grid_l_watts_body);
+        Decl decl_one_Grid = Grid.oneOf("g");
+        Decl decl_g_circuit_load = decl_one_Grid.get().join(field_circuit_GRID.join(field_load_SUPPLY)).oneOf("l");
+        Expr grid_l_watts_body = decl_g_circuit_load.get().join(field_watts_LOAD).sumOver(decl_g_circuit_load);
+        Func fun_grid_l_watts = new Func(null, "grid_l_watts", Util.asList(decl_one_Grid), SIGINT.oneOf(), grid_l_watts_body);
 
         /*fun grid_s_watts [g: Grid]: one Int
         *   sum s: g.circuit.supply | s.watts*/
 
-        Decl gs = Grid.join(circuit).join(supply).oneOf("s");
-        Expr grid_s_watts_body = supply_watts.sumOver(gs, g);
-        Func fun_grid_s_watts = new Func(null, "grid_s_watts", Util.asList(g), SIGINT.oneOf(), grid_s_watts_body);
+        Decl decl_g_circuit_supply = decl_one_Grid.get().join(field_circuit_GRID).join(field_supply_LOAD).oneOf("s");
+        Expr grid_s_watts_body = decl_g_circuit_supply.get().join(field_watts_SUPPLY).sumOver(decl_g_circuit_supply);
+        Func fun_grid_s_watts = new Func(null, "grid_s_watts", Util.asList(decl_one_Grid), SIGINT.oneOf(), grid_s_watts_body);
 
         /*fun load_watts [l: Load]: one Int
         *   sum l.watts*/
-        Decl lw = Load.join(load_watts).oneOf("lw");
-        Expr load_watts_body = load_watts.sumOver(lw, l);
-        Func fun_load_watts = new Func(null, "load_watts", Util.asList(l), SIGINT.oneOf(), load_watts_body);
+
+        Decl decl_oneOf_Load = Load.oneOf("l");
+        Expr load_watts_body = decl_oneOf_Load.get().join(field_watts_LOAD).sumOver(decl_oneOf_Load);
+        Func fun_load_watts = new Func(null, "load_watts", Util.asList(decl_oneOf_Load), SIGINT.oneOf(), load_watts_body);
 
         /*fun supply_watts [s: Supply]: one Int
-        *   sum s.watts*/
-        Decl sw = Supply.join(supply_watts).oneOf("sw");
-        Expr supply_watts_body = supply_watts.sumOver(sw, s);
-        Func fun_supply_watts = new Func(null, "supply_watts", Util.asList(s), SIGINT.oneOf(), supply_watts_body);
+           sum s.watts*/
+        Decl decl_oneOf_Supply = Supply.oneOf("s");
+        Expr supply_watts_body = decl_oneOf_Supply.get().join(field_watts_SUPPLY).sumOver(decl_oneOf_Supply);
+        Func fun_supply_watts = new Func(null, "supply_watts", Util.asList(decl_oneOf_Supply), SIGINT.oneOf(), supply_watts_body);
 
 
 
@@ -170,55 +157,73 @@ public class GridMetamodel {
 
 
 
+        Decl decl_OneOf_Load = Load.oneOf("l");
+        //all l: Load | (l.watts >= 0) and (l.watts =< 10)
+        fact = decl_OneOf_Load.get().join(field_watts_LOAD).gte(ExprConstant.makeNUMBER(0)).forAll(decl_OneOf_Load).and(fact);
+        fact = decl_OneOf_Load.get().join(field_watts_LOAD).lte(ExprConstant.makeNUMBER(10)).forAll(decl_OneOf_Load).and(fact);
 
-        /*all l: Load | (l.watts >= 0) and (l.watts =< 10)*/
-        fact = l.get().join(load_watts.cast2int()).gte(ExprConstant.makeNUMBER(0)).forAll(l).and(fact);
-        fact = l.get().join(load_watts).lte(ExprConstant.makeNUMBER(10)).forAll(l).and(fact);
+        //all s: Supply | (s.watts >= 0) and (s.watts =< 10)
+        Decl decl_OneOf_Supply = Supply.oneOf("s");
+        fact = decl_OneOf_Supply.get().join(field_watts_SUPPLY).gte(ExprConstant.makeNUMBER(0)).forAll(decl_OneOf_Supply).and(fact);
+        fact = decl_OneOf_Supply.get().join(field_watts_SUPPLY).lte(ExprConstant.makeNUMBER(10)).forAll(decl_OneOf_Supply).and(fact);
 
-        /*all s: Supply | (s.watts >= 0) and (s.watts =< 10)*/
-        fact = s.get().join(supply_watts).gte(ExprConstant.makeNUMBER(0)).forAll(s).and(fact);
-        fact = s.get().join(supply_watts).lte(ExprConstant.makeNUMBER(10)).forAll(s).and(fact);
+        //no g: Grid | g.circuit = g.circuit.nexts
+        Decl decl_OneOf_Grid = Grid.oneOf("g");
+        //fact = decl_OneOf_Grid.get().join(field_circuit_GRID).equal(decl_OneOf_Grid.get().join(field_circuit_GRID.nexts)).forNo(decl_OneOf_Grid).and(fact);
 
-        /*no g: Grid | g.circuit = g.circuit.nexts*/
-        //fact = g.get().join(circuit).equal(g.get().join(circuit.nexts)).forNo(g).and(fact);
+        //all g: Grid | g.circuit = Circuit
+        fact = decl_OneOf_Grid.get().join(field_circuit_GRID).equal(Circuit).forAll(decl_OneOf_Grid).and(fact);
 
-        /*all g: Grid | g.circuit = Circuit*/
-        fact = g.get().join(circuit).equal(Circuit).forAll(g).and(fact);
+        //no g: Grid | (g.grid_l_watts > g.grid_s_watts)
+        fact = decl_OneOf_Grid.get().join(fun_grid_l_watts.getBody().gt(fun_grid_s_watts.getBody())).forNo(decl_one_Grid).and(fact);
 
-        /*no g: Grid | (g.grid_l_watts > g.grid_s_watts)*/
-        fact = g.get().join(fun_grid_l_watts.getBody().gt(fun_grid_s_watts.getBody())).forNo(g).and(fact);
+        //all c: Component | #c.containing_circuit > 0
+        Decl decl_oneOf_Component = Component.oneOf("c");
+        fact = (decl_oneOf_Component.get().join(field_containing_circuit_COMPONENT)).cardinality().gt(ExprConstant.makeNUMBER(0)).forAll(decl_oneOf_Component).and(fact);
 
+        //all s: Circuit.supply | (s.load.watts =< s.watts)
+        Decl decl_oneOf_Circuit_supply = Circuit.join(field_supply_circuit_LOAD_CIRCUIT).oneOf("s");
+        fact = decl_oneOf_Circuit_supply.get().join(field_load_SUPPLY).join(field_watts_LOAD).gte(decl_oneOf_Circuit_supply.get().join(field_watts_SUPPLY)).forAll(decl_oneOf_Circuit_supply).and(fact);
 
-        /*all c: Component | #c.containing_circuit > 0*/
-        Decl c = Component.oneOf("c");
-        fact = (c.get().join(containing_circuit)).cardinality().gt(ExprConstant.makeNUMBER(0)).forAll(c).and(fact);
-
-        /*all s: Circuit.supply | (s.load.watts =< s.watts)*/
-
-        Decl Circuit_supply = Circuit.join(supply).oneOf("Circuit.supply");
-        fact = Circuit_supply.get().join(load).join(load_watts).gte(Circuit_supply.get().join(supply_watts)).forAll(Circuit_supply).and(fact);
-
-        /*all l: Circuit.load, s: Circuit.supply | ((s.supply_watts - s.load.load_watts) >= l.watts) <=> (l in s.load)*/
-        Decl Circuit_load = Circuit.join(load).oneOf("Circuit.load");
-        fact = ((Circuit_supply.get().join(supply_watts).minus(Circuit_supply.get().join(load).join(load_watts)).gte(Circuit_load.get().join(load_watts))).iff(l.get().in(Circuit_supply.get().join(load)))).and(fact);
-
-
-        /*all l: Load_Circuit, s: Supply_Circuit | ((s.s_watts - s.l_watts) >= (l.s_watts - l.l_watts)) <=> (l in s.load_circuit)*/
-        fact = ((Circuit_supply.get().join(supply_watts).minus(Circuit_supply.get().join(load_watts)).gte(Circuit_load.get().join(supply_watts).minus(Circuit_load.get().join(load_watts)))).iff(l.get().in(Circuit_supply.get().join(load_circuit)))).and(fact);
-
-        Func simplePred = new Func(null, "Simple_Predicate", Util.asList(g), null, circuit);
-
-        Expr testBody = simplePred.call(Grid);
+        //all l: Circuit.load, s: Circuit.supply | ((s.supply_watts - s.load.load_watts) >= l.watts) <=> (l in s.load)
+        Decl decl_oneOf_Circuit_load = Circuit.join(field_load_circuit_SUPPLY_CIRCUIT).oneOf("l");
+        fact = (decl_oneOf_Circuit_supply.get().join(field_watts_SUPPLY).minus(decl_oneOf_Circuit_supply.get().join(field_load_SUPPLY.join(field_watts_LOAD)))
+                .gte(decl_oneOf_Circuit_load.get().join(field_watts_LOAD)).iff(decl_oneOf_Circuit_load.get().in(decl_oneOf_Circuit_supply.get().join(field_load_SUPPLY))))
+                .forAll(decl_oneOf_Circuit_load, decl_oneOf_Circuit_supply).and(fact);
 
 
-        List<Sig> sigs = Util.asList(Grid, Circuit, SupplyCircuit, LoadCircuit, Component, Load, Supply, Switch, GP, SP, Wind, Geo, Hydro);
+        //all l: Load_Circuit, s: Supply_Circuit | ((s.s_watts - s.l_watts) >= (l.s_watts - l.l_watts)) <=> (l in s.load_circuit)
+        Decl decl_load_circuit = LoadCircuit.oneOf("load_circuit");
+        Decl decl_supply_circuit = SupplyCircuit.oneOf("supply_circuit");
+
+        fact = ((decl_supply_circuit.get().
+                join(fun_s_watts.call(decl_supply_circuit.get()))
+                .minus(decl_supply_circuit.get().
+                        join(fun_l_watts.call(decl_supply_circuit.get()))
+                ).gte(decl_load_circuit.get().join(fun_s_watts.call(decl_load_circuit.get())
+                .minus(decl_load_circuit.get().join(fun_l_watts.call(decl_load_circuit.get()))))
+                .iff(decl_load_circuit.get().in(decl_supply_circuit.get().join(field_load_circuit_SUPPLY_CIRCUIT))))));
 
 
-        Command test = new Command(false, 3, 8, 4, testBody);
+        Func someGrid = new Func(null, "SomeG", null, null, Grid.some());
+
+        // pred atMostThree[x:univ, y:univ] { #(x+y) >= 3 }
+        Decl decl_loads = Load.someOf("l");
+        Decl decl_supplies = Supply.someOf("s");
+        Expr body = decl_loads.get().plus(decl_supplies.get()).cardinality().lte(ExprConstant.makeNUMBER(3));
+        Func atMost3 = new Func(null, "atMost3", Util.asList(decl_loads,decl_supplies), null, body);
+
+        List<Sig> sigs = Arrays.asList(new Sig[] {Grid, Circuit, SupplyCircuit, LoadCircuit, Component, Load, Supply, Switch, GP, SP, Wind, Geo, Hydro});
+
+        // run { some A && atMostThree[B,B] } for 3 but 3 int, 3 seq
+        Expr expr1 = Circuit.some().and(atMost3.call(SP,SP));
+        Command cmd1 = new Command(false, 3, 8, 7, expr1);
+        A4Solution sol1 = TranslateAlloyToKodkod.execute_command(NOP, sigs, cmd1, options);
+        System.out.println("[Solution1]:");
+        System.out.println(sol1.toString());
 
 
-
-        A4Solution solution = TranslateAlloyToKodkod.execute_command(NOP, sigs, test, options);
+        A4Solution solution = TranslateAlloyToKodkod.execute_command(NOP, sigs, cmd1, options);
 
         while(solution.satisfiable()){
             System.out.println("[Solution]:");
